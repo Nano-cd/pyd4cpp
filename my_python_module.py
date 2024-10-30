4248df6fd9987433fbb05b629e820fea8b4f6eba
Di Cheng <72659127+Nano-cd@users.noreply.github.com>
2024-10-30 16:24:07 +0800

    Create my_python_module.py

---
+++ my_python_module.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def process_data(file_path):
+  #define your code here
+    x = [0.001, 1.312, 2.566, 5.113, 10.26, 20.54]
+    y = [657, 779, 831, 970, 1239, 1786]
+    wt = [1, 1, 1, 1, 1, 1]
+    a, b, c, d = outcome[0], outcome[1], outcome[2], outcome[3]
+    return a,b,c,d
